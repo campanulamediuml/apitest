1d2a54f257341d3d08e47d476cffff9972eaf90e
HeJingjing <hyz_h@outlook.com>
2017-12-19 10:18:59 +0800

    emmmm修改了一下

--- __etc__/database_test.py
+++
@@ -1,4 +0,0 @@
-import MySQLdb as mydatabase
-
-conn = mydatabase.connect(host=db.host, port=db.port, user=db.user, passwd=db.passwd, db=db.db, charset=db.charset)
-cursor = conn.cursor()
--- __etc__/request_test.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = 'http://localhost:8888/post_test_1'
-content = {'content':'just a content'}
-r = requests.post(url,content)
-print(r.text)
